714dca1e1524676669a35c89932478c1d6bf0401
anika57 <anika05022001@gmail.com>
2025-06-21 14:01:26 +0530

    changes

--- src/MainSection/MainSection.js
+++ src/MainSection/MainSection.js
@@ -8,7 +8,7 @@ function MainSection() {
             <div className="upper-heading">
                 <p>BEST SKINCARE OF THE YEAR</p>
             </div>
-            <img src="/assets/Group 27.png" alt="Radiant Skin" className="background-img"/>
+            <img src={`${process.env.PUBLIC_URL}/assets/Group 27.png`} alt="Radiant Skin" className="background-img"/>
             <div class="heading">
                 <p className="main-heading">ACHIEVE RADIANT SKIN WITH THE POWER OF NATURE’S FINEST INGREDIENTS.</p>
                 <p className="sub-heading">Discover the perfect blend of nature and science, crafted to nourish, rejuvenate, and enhance your skin’s natural beauty.</p>
